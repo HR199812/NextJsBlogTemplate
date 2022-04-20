c74896c15614c51417a29785ae1ee403c38e47fe
hritwik199812 <hritwik72@gmail.com>
2022-04-20 19:12:31 +0530

    Add Server Code

--- server/server.js
+++ server/server.js
@@ -1,3 +1,32 @@
 const mongoose = require("mongoose");
 const cors = require("cors");
 const express = require("express");
+
+require("dotenv").config();
+
+const app = express();
+const port = process.env.PORT || 5000;
+app.use(cors());
+app.use(express.json());
+
+mongoose
+  .connect(config.get("mongoURI"), {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("Connection to Atlas is successful");
+  })
+  .catch((error) => {
+    console.log(error);
+  });
+
+const userRouter = require("./routes/userRoute");
+const articleRouter = require("./routes/articleRoute");
+
+app.use("/User", userRouter);
+app.use("/Article", articleRouter);
+
+app.listen(port, () => {
+  console.log(`server is running on port ${port}`);
+});
