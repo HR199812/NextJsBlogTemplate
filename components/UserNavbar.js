fe1d5ac198e106bcbca34fc00b56f2fa0b992561
Hritwik Agarwal <39455725+HR199812@users.noreply.github.com>
2022-05-09 13:58:56 +0530

    Add User Navbar

---
+++ components/UserNavbar.js
@@ -0,0 +1,28 @@
+import LINK from "next/link";
+import styles from "../styles/Home.module.css";
+
+const UserNavbar = () => {
+  return (
+    <>
+      <div className={styles.navbar}>
+        <div className={styles.floatDivOne}>
+          <LINK href="/">
+            <p className={styles.logo}>Next Js Blog</p>
+          </LINK>
+        </div>
+        <div className={styles.floatDivTwo}>
+          <ul className="navbar-links">
+            <LINK href="/User/logout">
+              <li>Sign-Out</li>
+            </LINK>
+            <LINK href="/blog">
+              <li>Profile</li>
+            </LINK>
+          </ul>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default UserNavbar;
